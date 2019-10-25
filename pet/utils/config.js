d4deacab059bd6032cbc92cd391d971de43bbf25
Jingbo@li <18811145767@163.com>
2019-10-25 10:22:57 +0800

    201910251022
    1 收件人不能确认收货

--- pet/utils/config.js
+++ pet/utils/config.js
@@ -4,17 +4,17 @@
  */
 /** =================================================== */
 const Service_Phone = "4007778889"; // 客服电话
-const Version_Name = "1.3.24"; // 版本名称
-const Version_Code = 34; // 版本编号
+const Version_Name = "1.3.25"; // 版本名称
+const Version_Code = 35; // 版本编号
 
 /** =================================================== */
 /** 
  *                      网络请求
  */
 /** =================================================== */
-const URL_Service = "https://pet.tyferp.com"; // 路径 
+// const URL_Service = "https://pet.tyferp.com"; // 路径 
 // const URL_Service = "http://huji820.oicp.net"; // 路径
-// const URL_Service = "http://192.168.3.111:5050"; // 路径
+const URL_Service = "http://192.168.3.111:5050"; // 路径
 // const URL_Service = "http://192.168.3.233:5050"; // 路径
 
 const URL_Register = "/api/customer/"; // 注册
